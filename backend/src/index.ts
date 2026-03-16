3e54dc2305845e3c923aff91fadbead0c99811dd
Salwa-pratama <nandev1915@gmail.com>
2026-03-16 12:03:10 +0700

    masih strugling dengan configurasi prisma orm

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,7 @@ import { testSupabaseConnection } from "./database/supabase";
 const PORT = process.env.APP_PORT || 1915;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`-> Server running on port http://localhost:${PORT}`);
   // Untuk ngetes koneksi
   testSupabaseConnection();
 });
