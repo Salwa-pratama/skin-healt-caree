2e5f0e2ca5cde7b66b9970905de8629236b73ec7
rukuse <mismunir4747@gmail.com>
2026-05-17 14:42:12 +0700

    feat: menyelesaikan API history (get by id, update, dan hapus gambar cloudinary)

--- backend/src/modules/feature/history/repository.ts
+++ backend/src/modules/feature/history/repository.ts
@@ -14,11 +14,78 @@ export class HistoryRepository {
   async getHistoryAsync(userId: number): Promise<HistoryResponse[]> {
     return prisma.acneProblem.findMany({
       where: { userId: userId },
-      orderBy: { createdAt: 'desc' }
+      orderBy: { createdAt: 'desc' },
+      include: {
+        acneProblemSolutions: {
+          include: {
+            acneSolution: {
+              include: {
+                goodIngredient: true,
+                badIngredient: true,
+                habits: true,
+                treatments: true
+              }
+            }
+          }
+        }
+      }
+    });
+  }
+
+  async getHistoryByIdAsync(userId: number, id: number) {
+    return prisma.acneProblem.findFirst({
+      where: { id: id, userId: userId },
+      include: {
+        acneProblemSolutions: {
+          include: {
+            acneSolution: {
+              include: {
+                goodIngredient: true,
+                badIngredient: true,
+                habits: true,
+                treatments: true
+              }
+            }
+          }
+        }
+      }
+    });
+  }
+
+  async deletePivotRecords(id: number) {
+    return prisma.acneProblemSolution.deleteMany({
+      where: { acneProblemId: id }
+    });
+  }
+
+  async updateHistoryAsync(id: number, updateData: any) {
+    return prisma.acneProblem.update({
+      where: { id: id },
+      data: updateData,
+      include: {
+        acneProblemSolutions: {
+          include: {
+            acneSolution: {
+              include: {
+                goodIngredient: true,
+                badIngredient: true,
+                habits: true,
+                treatments: true
+              }
+            }
+          }
+        }
+      }
     });
   }
 
   async deleteHistoryAsync(id: number) {
+    // Hapus data di tabel pivot (AcneProblemSolution) terlebih dahulu untuk menghindari Foreign Key Constraint Error
+    await prisma.acneProblemSolution.deleteMany({
+      where: { acneProblemId: id }
+    });
+
+    // Setelah pivot dihapus, baru aman untuk menghapus AcneProblem
     return prisma.acneProblem.delete({
       where: { id: id }
     });
